509483731defe1e55d4f12ca202b37e0046b7c79
Nabila Ahmed <nabila.ahmed63@gmail.com>
2017-11-11 21:33:33 +0200

    :books: Uploading assets

---
+++ Assets/Scripts/CarsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//css_import PlayerScript;
+
+public class CarsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision c){
+		if (!PlayerScript.IsMuted())
+			GetComponent<AudioSource> ().Play ();
+		PlayerScript.GameIsover ();
+	}
+}
